f2d74a77e33be8aec55aa52b74c70ef0b46e9b05
[hnakae] <[nakae27@gmail.com]>
2018-11-22 01:03:41 -0800

    add game end alert

---
+++ src/app/game/chess.component.ts
@@ -0,0 +1,226 @@
+//
+// chess.component.ts
+
+export type Color = 'black' | 'white';
+
+export type PieceKind = 'pawn' | 'rook' | 'knight' | 'bishop' | 'king' | 'queen';
+
+export class State {
+    constructor(public color: Color, public pieceKind: PieceKind) {}
+}
+
+export type Result = -1 | 0 | 1;
+
+export type Delta = [number, number];
+export type Position = [number, number];
+
+// "abstract class"
+export abstract class Piece {
+    constructor(public kind: PieceKind) {}
+
+    abstract moves(pos: Position): Position[];
+
+    isValidPosition(pos: Position): boolean {
+        const x = pos[0];
+        const y = pos[1];
+        return ((x >= 0 && x <= 7) && (y >= 0 && y <= 7));
+    }
+
+    pos_add(pos: Position, delta: Delta): Position {
+        const next_pos: Position = [pos[0] + delta[0], pos[1] + delta[1]];
+        if (this.isValidPosition(next_pos)) {
+            return next_pos;
+        }
+        return null;
+    }
+    add(pos: Position, delta: Delta, positions: Position[]) {
+        const new_pos = this.pos_add(pos, delta);
+        if (new_pos) {
+            positions.push(new_pos);
+        }
+    }
+}
+
+export class King extends Piece {
+    constructor() { 
+        super('king');
+    }
+    moves(pos: Position): Position[] {
+        const positions = [];
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) {
+                    continue;
+                }
+                this.add(pos, [i, j], positions);
+            }
+        }
+        return positions;
+    }
+}
+
+export class Queen extends Piece {
+    constructor() {
+        super('queen');
+    }
+    moves(pos: Position): Position[] {
+        const positions = [];
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) {
+                    continue;
+                }
+                for (let n = 1; n <= 7; n++) {
+                    const x = n * i;
+                    const y = n * j;
+                    this.add(pos, [x, y], positions);
+                }
+            }
+        }
+        return positions;
+    }
+}
+
+export class Pawn extends Piece {
+    constructor() {
+        super('pawn');
+    }
+    moves(pos: Position): Position[] {
+        const positions = [];
+        this.add(pos, [0, 1], positions);
+        return positions;
+    }
+}
+
+export class Rook extends Piece {
+    constructor() {
+        super('rook');
+    }
+    moves(pos: Position): Position[] {
+        const positions = [];
+        for (let i = -1; i <= 1; i++) {
+            const js = (i === 0) ? [1, -1] : [0];
+            for (const j of js) {
+                for (let n = 1; n <= 7; n++) {
+                    const x = n * i;
+                    const y = n * j;
+                    this.add(pos, [x, y], positions);
+                }
+            }
+        }
+        return positions;
+    }
+}
+
+export class Bishop extends Piece {
+    constructor() {
+        super('bishop');
+    }
+    moves(pos: Position): Position[] {
+        const positions = [];
+        for (let i = -1; i <= 1; i += 2) {
+            for (let j = -1; j <= 1; j += 2) {
+                for (let n = 1; n <= 7; n++) {
+                    const x = n * i;
+                    const y = n * j;
+                    this.add(pos, [x, y], positions);
+                }
+            }
+        }
+        return positions;
+    }
+}
+
+class PiecePosition {
+    constructor(public color: Color, public kind: PieceKind, public pos: Position) {
+    }
+}
+
+export class Board {
+
+    board: State[][];
+
+    constructor() {
+        const board = [];
+        function createRow(rowIdx) {
+            const row = [];
+            for (let colIdx = 0; colIdx < 8; colIdx++) {
+                row.push(null);
+            }
+            return row;
+        }
+        for (let rowIdx = 0; rowIdx < 8; rowIdx++) {
+            const row = createRow(rowIdx);
+            board.push(row);
+        }
+        this.board = board;
+        this.init();
+    }
+
+    // put0(pp: PiecePosition) {
+    //     const state = new State(color, piece.kind);
+    //     this.setState(pos, state);
+    // }
+
+    put(color: Color, pieceKind: PieceKind, pos: Position): Color {
+        const oldState = this.getState(pos);
+        if (oldState !== null && oldState.color === color) {
+            throw new Error('invalid move!');
+        }
+        if (oldState.pieceKind === 'king') {
+            return color; // return winner's color.
+        }
+        const state = new State(color, pieceKind);
+        this.setState(pos, state);
+        return null;
+    }
+
+    getState0(rowIdx: number, colIdx: number): State {
+        return this.board[rowIdx][colIdx];
+    }
+
+    getState(pos: Position): State {
+        return this.board[pos[0]][pos[1]];
+    }
+
+    setState(pos: Position, state: State) {
+        this.board[pos[0]][pos[1]] = state;
+    }
+
+    nextMoves(color: Color, piece: Piece, pos: Position): Position[] {
+        const nextPositions = [];
+        for (const new_pos of piece.moves(pos)) {
+            if (this.validBoardPosition(color, new_pos)) {
+                nextPositions.push(new_pos);
+            }
+        }
+        return nextPositions;
+    }
+
+    validBoardPosition(color: Color, pos: Position) {
+        const state = this.getState(pos);
+        return state.color === null || state.color !== color;
+    }
+
+    init() {
+        this.setInitPositions('white');
+        this.setInitPositions('black');
+    }
+
+    setInitPositions(color: Color) {
+        const x = ((color === 'white') ? 0 : 7);
+        this.put(color, new Rook(), [0, x]);
+        this.put(color, new Rook(), [7, x]);
+        this.put(color, new Knight(), [1, x]);
+        this.put(color, new Knight(), [6, x]);
+        this.put(color, new Bishop(), [2, x]);
+        this.put(color, new Bishop(), [5, x]);
+        this.put(color, new Queen(), [3, x]);
+        this.put(color, new King(), [4, x]);
+
+        const y = (color === 'white') ? 1 : 7;
+        for (let i = 0; i < 8; i++) {
+            this.put(color, new Pawn(), [i, y]);
+        }
+    }
+}
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,139 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Board, State, Color, Position, Piece, PieceKind } from './chess.component';
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  board: Board;
+  color: Color;
 
-  constructor() { }
+  //state: State;
+  // simulator: OthelloSimulator = null;
+  //audio = null;
+
+  constructor() {
+    this.board = new Board();
+  }
+    // this.audio = new Audio('../../assets/sound/POL-air-ducts-short.wav');
+    // if (typeof this.audio.loop == 'boolean') {
+    //   this.audio.loop = true;
+    // } else {
+    //     this.audio.addEventListener('ended', function () {
+    //     this.currentTime = 0;
+    //     this.play();
+    //   }, false);
+    // }
+
 
   ngOnInit() {
+    this.reset();
   }
 
+  reset() {
+    this.board.init();
+    this.color = 'black';
+    // this.simulator = null;
+    // this.audio.pause();
+    // this.audio.currentTime = 0;
+  }
+
+  isWhite(rowIdx, colIdx) {
+    const state = this.board.getState0(rowIdx, colIdx);
+    if (!state) {
+      return false;
+    }
+    return state.color === 'white';
+  }
+  isBlack(rowIdx, colIdx) {
+    const state = this.board.getState0(rowIdx, colIdx);
+    if (!state) {
+      return false;
+    }
+    return state.color === 'black';
+  }
+  isEmpty(rowIdx, colIdx) {
+    const state = this.board.getState0(rowIdx, colIdx);
+    return state === null;
+  }
+
+  userPickUpPiece(color: Color, piece: Piece, currentPos: Position, nextPos: Position) {
+     const state = this.board.getState(currentPos);
+    if (state === null || state.color !== color) {
+      alert('invalid move!');
+      return;
+    }
+    try {
+      let found = false;
+      for (const newPos of this.board.nextMoves(color, piece, currentPos)) {
+        if (newPos[0] === nextPos[0] && newPos[1] === nextPos[1]) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        alert('invalid move!');
+        return;
+      }
+      const w = this.board.put(color, piece.kind, nextPos);
+      this.board.setState(currentPos, null);
+
+      if (w === null) {
+        this.nextColor();
+      } else {
+        alert('Game over: player '+w+' wins.');
+      }
+    } catch (err) {
+      alert('invalid location! ' + err);
+    }
+  }
+
+  private nextColor() {
+    this.color = (this.color === 'white') ? 'black' : 'white';
+  }
 }
+
+
+/* CHESS PSEUDOCODE
+     ----- game.component.html pseudocode -----
+  When clicked {
+    // CALLS GAME LOGIC
+    instantiate piece when you create board.
+
+    if there is a piece here
+      check piece type.
+        if piece.type==rook-etc
+          pick up piece (create temp and remove from board) (click)="pickUpPiece"
+          highlight possible moves and enemy pieces u can capture
+          wait for valid click
+            take temp and place on board.
+              if location has enemy piece,
+                remove enemy piece and add to captured pile
+  }
+
+     ------ game.component.ts pseudocode-----------
+     // GAME LOGIC
+     this.board = new Board();
+      pickUpPiece() { 
+        piece = new Piece();
+        pick up; 
+        this.board.putDown(piece)
+        if(xyz){
+          capture();
+        }
+      }
+      capture() {  }
+
+   ------ chess.component.ts pseudocode-----------
+   // OBJECTS FOR GAME LOGIC
+   export state //black or white
+   export pieces
+   export board with pieces initialized?
+   export stuff
+
+
+*/
+
